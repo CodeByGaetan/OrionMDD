1d91ef1d2bce37fc72c341ebc4882adb2ff302a3
CodeByGaetan <codebygaetan@gmail.com>
2023-12-18 11:49:38 +0100

    ajout de l'attribut de relation posts à user

--- back/src/main/java/com/openclassrooms/mddapi/models/User.java
+++ back/src/main/java/com/openclassrooms/mddapi/models/User.java
@@ -1,9 +1,16 @@
 package com.openclassrooms.mddapi.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.Email;
@@ -33,5 +40,9 @@ public class User {
     @NonNull
     @Size(min = 8)
     private String password;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private List<Post> posts = new ArrayList<>();
 
 }
