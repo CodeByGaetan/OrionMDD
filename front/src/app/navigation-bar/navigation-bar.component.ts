a9350991e680be909d1370085de1b58390c61dcb
CodeByGaetan <codebygaetan@gmail.com>
2024-01-03 18:29:37 +0100

    correction de styles sur navigation bar

--- front/src/app/navigation-bar/navigation-bar.component.ts
+++ front/src/app/navigation-bar/navigation-bar.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
   styleUrls: ['./navigation-bar.component.scss']
 })
-export class NavigationBarComponent {
-  
+export class NavigationBarComponent implements OnInit {
+
+  public screenWidth!: number;
+
+  constructor(public router: Router) { }
+
+  ngOnInit() {
+    this.screenWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event']) 
+  public getScreenSize(event: Event) {
+    this.screenWidth = window.innerWidth;
+  }
+
+  showToolbar(): boolean {
+    const cond1 = this.router.url === '/';
+    const cond2 = this.router.url === '/signin' && this.screenWidth < 600;
+    const cond3 = this.router.url === '/signup' && this.screenWidth < 600;
+    if (cond1 || cond2 || cond3) {
+      return false;
+    }
+    return true;
+  }
+
+  showNavLinks(): boolean {
+    return this.router.url !== '/signin' && this.router.url !== '/signup';
+  }
+
 }
